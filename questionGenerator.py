573dc65a424963e80b78bf33bb68e127eca516b4
J <jon.kearney@gmail.com>
2017-11-14 09:36:04 -0800

    changing to urllib3

--- questionGenerator.py
+++ questionGenerator.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import dbManager
 import logging
-import urllib2
+import urllib3
 import json
 import gameManager
 import questionGenerator
@@ -17,10 +17,13 @@ app = Flask(__name__)
 
 def generateAndSaveQuestions(game_id, number_of_questions):
         query_string = "http://www.jservice.io/api/random?count={}".format(number_of_questions)
-        json_string = urllib2.urlopen(query_string).read()
+        #json_string = urllib2.urlopen(query_string).read()
+        http = urllib3.PoolManager()
+        json_string = http.request("GET",  query_string)
         print("Getting questions and getting answers begrudgingly from Trebek...")
 
-        parsed_json = json.loads(json_string)
+        #parsed_json = json.loads(json_string)
+        parsed_json = json.loads(json_string.data)
         for i in range (0,number_of_questions):
             #Add question to the DB
             question_id = dbManager.addQuestion(game_id, i, parsed_json[i]["question"],parsed_json[i]["answer"])
